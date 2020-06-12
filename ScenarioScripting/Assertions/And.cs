2acf4abd97b36b112f142ae84b2a46f039a8c91a
Remi Pelletier <remipelletier5@gmail.com>
2020-06-12 16:09:32 -0700

    Added AndAssertion and OrAssertion

---
+++ ScenarioScripting/Assertions/And.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ScenarioScripting.Assertions
+{
+    public class AndAssertion : IAssertion
+    {
+        private IAssertion First { get; set; }
+        private IAssertion Second { get; set; }
+
+        public AndAssertion(IAssertion first, IAssertion second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public bool Assert()
+        {
+            return First.Assert() && Second.Assert();
+        }
+    }
+}
---
+++ ScenarioScripting/Assertions/Or.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ScenarioScripting.Assertions
+{
+    public class OrAssertion : IAssertion
+    {
+        private IAssertion First { get; set; }
+        private IAssertion Second { get; set; }
+
+        public OrAssertion(IAssertion first, IAssertion second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public bool Assert()
+        {
+            return First.Assert() || Second.Assert();
+        }
+    }
+}
