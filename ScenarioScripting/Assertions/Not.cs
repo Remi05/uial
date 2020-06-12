cdcefa5ebd033f86fbfc366ce2c0d5b969376122
Remi Pelletier <remipelletier5@gmail.com>
2020-06-12 16:06:02 -0700

    Added Not assertion

---
+++ ScenarioScripting/Assertions/Not.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ScenarioScripting.Assertions
+{
+    public class Not : IAssertion
+    {
+        private IAssertion Assertion { get; set; } 
+
+        public Not(IAssertion assertion)
+        {
+            Assertion = assertion;
+        }
+
+        public bool Assert()
+        {
+            return !Assertion.Assert();
+        }
+    }
+}
