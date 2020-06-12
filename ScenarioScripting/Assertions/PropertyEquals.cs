85bfa3f64058794b031ea21cb2010113f817c9d0
Remi Pelletier <remipelletier5@gmail.com>
2020-06-12 16:42:39 -0700

    Added PropertyEquals assertion

---
+++ ScenarioScripting/Assertions/PropertyEquals.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Automation;
+using ScenarioScripting.Contexts;
+
+namespace ScenarioScripting.Assertions
+{
+    public class PropertyEquals : IAssertion
+    {
+        public string Name => "PropertyEquals";
+
+        private IContext Context { get; set; }
+        private AutomationProperty Property { get; set; }
+        private object Value { get; set; }
+
+        public PropertyEquals(IContext context, AutomationProperty property, object value)
+        {
+            Context = context;
+            Property = property;
+            Value = value;
+        }
+
+        public bool Assert()
+        {
+            if (Context?.RootElement == null)
+            {
+                return false;
+            }
+            object actualValue = Context.RootElement.GetCurrentPropertyValue(Property);
+            return Value == null && actualValue == null
+                || Value != null && Value.Equals(actualValue);
+        }
+    }
+}
