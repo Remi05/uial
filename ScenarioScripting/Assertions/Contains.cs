1f3c87b11e4e2786d71aa465118ad80ace97a500
Remi Pelletier <remipelletier5@gmail.com>
2020-06-12 16:16:37 -0700

    Added Contains and StartsWith assertions

---
+++ ScenarioScripting/Assertions/Contains.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ScenarioScripting.Assertions
+{
+    public class Contains : IAssertion
+    {
+        public string Name => "Contains";
+
+        private string First { get; set; }
+        private string Second { get; set; }
+
+        public Contains(string first, string second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public bool Assert()
+        {
+            return First != null && Second != null
+                && First.Contains(Second);
+        }
+    }
+}
---
+++ ScenarioScripting/Assertions/StartsWith.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ScenarioScripting.Assertions
+{
+    public class StartsWith : IAssertion
+    {
+        public string Name => "StartsWith";
+
+        private string First { get; set; }
+        private string Second { get; set; }
+
+        public StartsWith(string first, string second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public bool Assert()
+        {
+            return First != null && Second != null
+                && First.StartsWith(Second);
+        }
+    }
+}
