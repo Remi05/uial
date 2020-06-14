a0aeb02104db6719f15c7085725e837213cbfd23
Remi Pelletier <remipelletier5@gmail.com>
2020-06-13 23:31:03 -0700

    Added Unit Tests project

---
+++ ScenarioScriptingUnitTests/Scenarios/ScenarioDefinition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScenarioScriptingUnitTests
+{
+    [TestClass]
+    public class ScenarioDefinitionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
