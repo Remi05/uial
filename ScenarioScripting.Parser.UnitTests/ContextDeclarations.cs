9155e748d0bdfd497662e1d68282d6a3a11c2829
Remi Pelletier <remipelletier5@gmail.com>
2020-06-14 16:58:55 -0700

    Added ScenarioParser unit tests

---
+++ ScenarioScripting.Parser.UnitTests/ContextDeclarations.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScenarioScripting.Contexts;
+
+namespace ScenarioScripting.Parser.UnitTests
+{
+    [TestClass]
+    public class ContextDeclarationsParsingTests
+    {
+        private const string ContextIdentifier = "context";
+        private const string ContextName = "TestContext";
+        private const string SingleParam = "$testParam";
+        private const string Params = "$testParam1, $testParam2";
+        private const string SingleCondition = "Name=\"TestName\"";
+        private const string CompositeCondition = "Name=\"TestName\", ControlType=\"Button\"";
+
+        private const string Context_NoParamsNoCondition             = ContextIdentifier + " " + ContextName + ":";
+        private const string Context_SingleParam                     = ContextIdentifier + " " + ContextName + "(" + SingleParam + "):";
+        private const string Context_MultipleParams                  = ContextIdentifier + " " + ContextName + "(" + Params + "):";
+        private const string Context_SingleRootCondition             = ContextIdentifier + " " + ContextName + " [" + SingleCondition + "]:";
+        private const string Context_CompositeRootCondition          = ContextIdentifier + " " + ContextName + " [" + CompositeCondition + "]:";
+        private const string Context_SingleUniqueCondition           = ContextIdentifier + " " + ContextName + " {" + SingleCondition + "}:";
+        private const string Context_CompositeUniqueCondition        = ContextIdentifier + " " + ContextName + " {" + CompositeCondition + "}:";
+        private const string Context_RootAndUniqueCondition          = ContextIdentifier + " " + ContextName + " [" + CompositeCondition + "] {" + CompositeCondition + "}:";
+        private const string Context_ParamsAndRootCondition          = ContextIdentifier + " " + ContextName + "(" + Params + ") [" + CompositeCondition + "]:";
+        private const string Context_ParamsAndUniqueCondition        = ContextIdentifier + " " + ContextName + "(" + Params + ") {" + CompositeCondition + "}:";
+        private const string Context_ParamsAndRootAndUniqueCondition = ContextIdentifier + " " + ContextName + "(" + Params + ") [" + CompositeCondition + "] {" + CompositeCondition + "}:";
+
+        [TestMethod]
+        public void ContextNameIsParsed()
+        {
+            ScriptParser parser = new ScriptParser();
+            IContextDefinition contextDefinition = parser.ParseContextDefinitionDeclaration(null, Context_SingleRootCondition);
+
+            Assert.IsNotNull(contextDefinition, "The parsed IContextDefinition should not be null.");
+            Assert.AreEqual(ContextName, contextDefinition.Name, "The parsed IContextDefinition's Name should be the given name.");
+        }
+
+        [DataRow(Context_NoParamsNoCondition, DisplayName = "ContextCanBeParsed_NoParamsNoCondition")]
+        [DataRow(Context_SingleParam, DisplayName = "ContextCanBeParsed_SingleParam")]
+        [DataRow(Context_MultipleParams, DisplayName = "ContextCanBeParsed_MultipleParams")]
+        [DataRow(Context_SingleRootCondition, DisplayName = "ContextCanBeParsed_SingleRootCondition")]
+        [DataRow(Context_CompositeRootCondition, DisplayName = "ContextCanBeParsed_CompositeRootCondition")]
+        [DataRow(Context_SingleUniqueCondition, DisplayName = "ContextCanBeParsed_SingleUniqueCondition")]
+        [DataRow(Context_CompositeUniqueCondition, DisplayName = "ContextCanBeParsed_CompositeUniqueCondition")]
+        [DataRow(Context_RootAndUniqueCondition, DisplayName = "ContextCanBeParsed_RootAndUniqueCondition")]
+        [DataRow(Context_ParamsAndRootCondition, DisplayName = "ContextCanBeParsed_ParamsAndRootCondition")]
+        [DataRow(Context_ParamsAndUniqueCondition, DisplayName = "ContextCanBeParsed_ParamsAndUniqueCondition")]
+        [DataRow(Context_ParamsAndRootAndUniqueCondition, DisplayName = "ContextCanBeParsed_ParamsAndRootAndUniqueCondition")]
+        [DataTestMethod]
+        public void ContextCanBeParsed(string contextDeclaration)
+        {
+            ScriptParser parser = new ScriptParser();
+            IContextDefinition contextDefinition = parser.ParseContextDefinitionDeclaration(null, contextDeclaration);
+
+            Assert.IsNotNull(contextDefinition, "The parsed IContextDefinition should not be null.");
+        }
+    }
+}
---
+++ ScenarioScripting.Parser.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ScenarioScripting.Parser.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ScenarioScripting.Parser.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("07a59ce0-caef-4906-b057-d74179461265")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
