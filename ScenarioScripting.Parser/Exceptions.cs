5921c6d8643be045971a7358614b0c7d39c446c4
Remi Pelletier <remipelletier5@gmail.com>
2020-06-14 16:58:30 -0700

    Added ScriptParser exceptions

---
+++ ScenarioScripting.Parser/Exceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ScenarioScripting.Parser.Exceptions
+{
+    public class InvalidContextDeclarationException : Exception
+    {
+        private string ContextDeclaration { get; set; }
+
+        public override string Message => $"The given context declaration is invalid:\n{ContextDeclaration}";
+
+        public InvalidContextDeclarationException(string contextDeclaration)
+        {
+            ContextDeclaration = contextDeclaration;
+        }
+    }
+}
