671f7bbe75ad405381530aae9906271d431a4a75
Remi Pelletier <remipelletier5@gmail.com>
2020-06-14 02:20:40 -0700

    Removed unused usings

--- ScenarioScriptingUnitTests/Conditions/PropertyConditionDefinition.cs
+++ ScenarioScriptingUnitTests/Conditions/PropertyConditionDefinition.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Automation;
+﻿using System.Windows.Automation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ScenarioScripting;
 using ScenarioScripting.Conditions;
-using ScenarioScripting.Contexts;
-using ScenarioScriptingUnitTests.Interactions;
 
 namespace ScenarioScriptingUnitTests.Conditions
 {
