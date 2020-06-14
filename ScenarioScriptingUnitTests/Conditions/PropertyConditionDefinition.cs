8787fd4ab06cfc9d442031e78eaee16b28db9394
Remi Pelletier <remipelletier5@gmail.com>
2020-06-14 02:19:53 -0700

    Added PropertyConditionDefinition unit tests

---
+++ ScenarioScriptingUnitTests/Conditions/PropertyConditionDefinition.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScenarioScripting;
+using ScenarioScripting.Conditions;
+using ScenarioScripting.Contexts;
+using ScenarioScriptingUnitTests.Interactions;
+
+namespace ScenarioScriptingUnitTests.Conditions
+{
+    [TestClass]
+    public class PropertyConditionDefinitionTests
+    {
+        [TestMethod]
+        public void VerifyControlTypeConditionIsResolved()
+        {
+            AutomationProperty expectedProperty = AutomationElement.ControlTypeProperty;
+            ControlType expectedControlType = ControlType.Button;
+            ValueDefinition valueDefinition = ValueDefinition.FromLitteral("Button");
+
+            PropertyConditionDefinition propertyConditionDefinition = new PropertyConditionDefinition(expectedProperty, valueDefinition);
+            PropertyCondition actualCondition = propertyConditionDefinition.Resolve(null) as PropertyCondition;
+
+            Assert.IsNotNull(actualCondition);
+            Assert.AreEqual(expectedProperty, actualCondition.Property);
+            Assert.AreEqual(expectedControlType.Id, actualCondition.Value);
+        }
+
+        [TestMethod]
+        public void VerifyConditionIsResolved()
+        {
+            AutomationProperty expectedProperty = AutomationElement.NameProperty;
+            string expectedName = "TestControlName";
+            ValueDefinition valueDefinition = ValueDefinition.FromLitteral(expectedName);
+
+            PropertyConditionDefinition propertyConditionDefinition = new PropertyConditionDefinition(expectedProperty, valueDefinition);
+            PropertyCondition actualCondition = propertyConditionDefinition.Resolve(null) as PropertyCondition;
+
+            Assert.IsNotNull(actualCondition);
+            Assert.AreEqual(expectedProperty, actualCondition.Property);
+            Assert.AreEqual(expectedName, actualCondition.Value as string);
+        }
+    }
+}
