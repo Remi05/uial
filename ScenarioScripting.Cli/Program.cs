216e24d0987e31f92bb682e7c727e171d5edf67d
Remi Pelletier <remipelletier5@gmail.com>
2020-06-14 22:19:00 -0700

    Cleaned up CLI

--- ScenarioScripting.Cli/Program.cs
+++ ScenarioScripting.Cli/Program.cs
@@ -11,17 +11,51 @@ namespace ScenarioScripting.Cli
     {
         static void Main(string[] args)
         {
-            string filePath = "C:\\Users\\remi_\\Desktop\\launchteams.uial";
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Expected two arguments: <script_file_path> <scenario_name>");
+                return;
+            }
 
-            ScriptParser parser = new ScriptParser();
-            Script script = parser.ParseScript(filePath);
+            string scriptFilePath = args[0];
+            string scenarioName = args[1];
 
-            Console.Write("Enter scenario: ");
-            string scenarioName = Console.ReadLine();
-            RuntimeScope scope = new RuntimeScope(script.RootScope, new Dictionary<string, string>());
-            IContext rootContext = new RootContext(scope);
-            Scenario scenario = script.ScenarioDefinitions[scenarioName].Resolve(rootContext);
-            scenario.Do();
+            RunScenario(scriptFilePath, scenarioName);
+        }
+
+        private static void RunScenario(string scriptFilePath, string scenarioName)
+        {
+            var parser = new ScriptParser();
+            Script script;
+
+            try
+            {
+                script = parser.ParseScript(scriptFilePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred while parsing the given script:\n{e.Message}");
+                return;
+            }
+            
+              
+            if (!script.ScenarioDefinitions.ContainsKey(scenarioName))
+            {
+                Console.WriteLine($"Scenario \"{scenarioName}\" could not be found in the given script.");
+                return;
+            }
+
+            try
+            {
+                var scope = new RuntimeScope(script.RootScope, new Dictionary<string, string>());
+                var rootContext = new RootContext(scope);
+                Scenario scenario = script.ScenarioDefinitions[scenarioName].Resolve(rootContext);
+                scenario.Do();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occured while running the specified scenario:\n{e.Message}");
+            }
         }
     }
 }
