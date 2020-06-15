c3cdc2f6e5815f459d9347a7ba8c058020605729
Remi Pelletier <remipelletier5@gmail.com>
2020-06-14 17:58:56 -0700

    Grouped constants in classes

--- ScenarioScripting.Parser.UnitTests/ContextDeclarations.cs
+++ ScenarioScripting.Parser.UnitTests/ContextDeclarations.cs
@@ -9,54 +9,60 @@ namespace ScenarioScripting.Parser.UnitTests
     {
         private const string ContextIdentifier = "context";
         private const string ContextName = "TestContext";
-        private const string SingleParam = "$testParam";
+        private const string Param = "$testParam";
         private const string Params = "$testParam1, $testParam2";
         private const string SingleCondition = "Name=\"TestName\"";
         private const string CompositeCondition = "Name=\"TestName\", ControlType=\"Button\"";
 
-        private const string ValidContext_NoParamsNoCondition             = ContextIdentifier + " " + ContextName + ":";
-        private const string ValidContext_SingleParam                     = ContextIdentifier + " " + ContextName + "(" + SingleParam + "):";
-        private const string ValidContext_MultipleParams                  = ContextIdentifier + " " + ContextName + "(" + Params + "):";
-        private const string ValidContext_SingleRootCondition             = ContextIdentifier + " " + ContextName + " [" + SingleCondition + "]:";
-        private const string ValidContext_CompositeRootCondition          = ContextIdentifier + " " + ContextName + " [" + CompositeCondition + "]:";
-        private const string ValidContext_SingleUniqueCondition           = ContextIdentifier + " " + ContextName + " {" + SingleCondition + "}:";
-        private const string ValidContext_CompositeUniqueCondition        = ContextIdentifier + " " + ContextName + " {" + CompositeCondition + "}:";
-        private const string ValidContext_RootAndUniqueCondition          = ContextIdentifier + " " + ContextName + " [" + CompositeCondition + "] {" + CompositeCondition + "}:";
-        private const string ValidContext_ParamsAndRootCondition          = ContextIdentifier + " " + ContextName + "(" + Params + ") [" + CompositeCondition + "]:";
-        private const string ValidContext_ParamsAndUniqueCondition        = ContextIdentifier + " " + ContextName + "(" + Params + ") {" + CompositeCondition + "}:";
-        private const string ValidContext_ParamsAndRootAndUniqueCondition = ContextIdentifier + " " + ContextName + "(" + Params + ") [" + CompositeCondition + "] {" + CompositeCondition + "}:";
+        private class ValidContexts
+        {
+            public const string NoParamsNoCondition             = ContextIdentifier + " " + ContextName + ":";
+            public const string SingleParam                     = ContextIdentifier + " " + ContextName + "(" + Param + "):";
+            public const string MultipleParams                  = ContextIdentifier + " " + ContextName + "(" + Params + "):";
+            public const string SingleRootCondition             = ContextIdentifier + " " + ContextName + " [" + SingleCondition + "]:";
+            public const string CompositeRootCondition          = ContextIdentifier + " " + ContextName + " [" + CompositeCondition + "]:";
+            public const string SingleUniqueCondition           = ContextIdentifier + " " + ContextName + " {" + SingleCondition + "}:";
+            public const string CompositeUniqueCondition        = ContextIdentifier + " " + ContextName + " {" + CompositeCondition + "}:";
+            public const string RootAndUniqueCondition          = ContextIdentifier + " " + ContextName + " [" + CompositeCondition + "] {" + CompositeCondition + "}:";
+            public const string ParamsAndRootCondition          = ContextIdentifier + " " + ContextName + "(" + Params + ") [" + CompositeCondition + "]:";
+            public const string ParamsAndUniqueCondition        = ContextIdentifier + " " + ContextName + "(" + Params + ") {" + CompositeCondition + "}:";
+            public const string ParamsAndRootAndUniqueCondition = ContextIdentifier + " " + ContextName + "(" + Params + ") [" + CompositeCondition + "] {" + CompositeCondition + "}:";
+        }
 
-        private const string InvalidContext_MissingName                = ContextIdentifier + " :";
-        private const string InvalidContext_MultipleParamsDeclarations = ContextIdentifier + " " + ContextName + "(" + Params + ") (" + Params + "):";
-        private const string InvalidContext_MultipleRootConditions     = ContextIdentifier + " " + ContextName + " [" + SingleCondition + "] [" + SingleCondition + "]:";
-        private const string InvalidContext_MultipleUniqueConditions   = ContextIdentifier + " " + ContextName + " {" + SingleCondition + "} {" + SingleCondition + "}:";
-        private const string InvalidContext_ParamsAfterRootCondition   = ContextIdentifier + " " + ContextName + " [" + SingleCondition + "] (" + Params + "):";
-        private const string InvalidContext_ParamsAfterUniqueCondition = ContextIdentifier + " " + ContextName + " {" + SingleCondition + "} (" + Params + "):";
-        private const string InvalidContext_TextAfterColon             = ContextIdentifier + " " + ContextName + ": SomeTestText";
+        private class InvalidContexts
+        {
+            public const string MissingName                = ContextIdentifier + " :";
+            public const string MultipleParamsDeclarations = ContextIdentifier + " " + ContextName + "(" + Params + ") (" + Params + "):";
+            public const string MultipleRootConditions     = ContextIdentifier + " " + ContextName + " [" + SingleCondition + "] [" + SingleCondition + "]:";
+            public const string MultipleUniqueConditions   = ContextIdentifier + " " + ContextName + " {" + SingleCondition + "} {" + SingleCondition + "}:";
+            public const string ParamsAfterRootCondition   = ContextIdentifier + " " + ContextName + " [" + SingleCondition + "] (" + Params + "):";
+            public const string ParamsAfterUniqueCondition = ContextIdentifier + " " + ContextName + " {" + SingleCondition + "} (" + Params + "):";
+            public const string TextAfterColon             = ContextIdentifier + " " + ContextName + ": SomeTestText";
+        }
 
 
         [TestMethod]
         public void ContextNameIsParsed()
         {
             ScriptParser parser = new ScriptParser();
-            IContextDefinition contextDefinition = parser.ParseContextDefinitionDeclaration(null, ValidContext_SingleRootCondition);
+            IContextDefinition contextDefinition = parser.ParseContextDefinitionDeclaration(null, ValidContexts.SingleRootCondition);
 
             Assert.IsNotNull(contextDefinition, "The parsed IContextDefinition should not be null.");
             Assert.AreEqual(ContextName, contextDefinition.Name, "The parsed IContextDefinition's Name should be the given name.");
         }
 
 
-        [DataRow(ValidContext_NoParamsNoCondition, DisplayName = "ValidContext_NoParamsNoCondition")]
-        [DataRow(ValidContext_SingleParam, DisplayName = "ValidContext_SingleParam")]
-        [DataRow(ValidContext_MultipleParams, DisplayName = "ValidContext_MultipleParams")]
-        [DataRow(ValidContext_SingleRootCondition, DisplayName = "ValidContext_SingleRootCondition")]
-        [DataRow(ValidContext_CompositeRootCondition, DisplayName = "ValidContext_CompositeRootCondition")]
-        [DataRow(ValidContext_SingleUniqueCondition, DisplayName = "ValidContext_SingleUniqueCondition")]
-        [DataRow(ValidContext_CompositeUniqueCondition, DisplayName = "ValidContext_CompositeUniqueCondition")]
-        [DataRow(ValidContext_RootAndUniqueCondition, DisplayName = "ValidContext_RootAndUniqueCondition")]
-        [DataRow(ValidContext_ParamsAndRootCondition, DisplayName = "ValidContext_ParamsAndRootCondition")]
-        [DataRow(ValidContext_ParamsAndUniqueCondition, DisplayName = "ValidContext_ParamsAndUniqueCondition")]
-        [DataRow(ValidContext_ParamsAndRootAndUniqueCondition, DisplayName = "ValidContext_ParamsAndRootAndUniqueCondition")]
+        [DataRow(ValidContexts.NoParamsNoCondition,             DisplayName = "ValidContext_NoParamsNoCondition")]
+        [DataRow(ValidContexts.SingleParam,                     DisplayName = "ValidContext_SingleParam")]
+        [DataRow(ValidContexts.MultipleParams,                  DisplayName = "ValidContext_MultipleParams")]
+        [DataRow(ValidContexts.SingleRootCondition,             DisplayName = "ValidContext_SingleRootCondition")]
+        [DataRow(ValidContexts.CompositeRootCondition,          DisplayName = "ValidContext_CompositeRootCondition")]
+        [DataRow(ValidContexts.SingleUniqueCondition,           DisplayName = "ValidContext_SingleUniqueCondition")]
+        [DataRow(ValidContexts.CompositeUniqueCondition,        DisplayName = "ValidContext_CompositeUniqueCondition")]
+        [DataRow(ValidContexts.RootAndUniqueCondition,          DisplayName = "ValidContext_RootAndUniqueCondition")]
+        [DataRow(ValidContexts.ParamsAndRootCondition,          DisplayName = "ValidContext_ParamsAndRootCondition")]
+        [DataRow(ValidContexts.ParamsAndUniqueCondition,        DisplayName = "ValidContext_ParamsAndUniqueCondition")]
+        [DataRow(ValidContexts.ParamsAndRootAndUniqueCondition, DisplayName = "ValidContext_ParamsAndRootAndUniqueCondition")]
         [DataTestMethod]
         public void ValidContextsCanBeParsed(string contextDeclaration)
         {
@@ -67,13 +73,13 @@ namespace ScenarioScripting.Parser.UnitTests
         }
 
 
-        [DataRow(InvalidContext_MissingName, DisplayName = "InvalidContext_MissingName")]
-        [DataRow(InvalidContext_MultipleParamsDeclarations, DisplayName = "InvalidContext_MultipleParamsDeclarations")]
-        [DataRow(InvalidContext_MultipleRootConditions, DisplayName = "InvalidContext_MultipleRootConditions")]
-        [DataRow(InvalidContext_MultipleUniqueConditions, DisplayName = "InvalidContext_MultipleUniqueConditions")]
-        [DataRow(InvalidContext_ParamsAfterRootCondition, DisplayName = "InvalidContext_ParamsAfterRootCondition")]
-        [DataRow(InvalidContext_ParamsAfterUniqueCondition, DisplayName = "InvalidContext_ParamsAfterUniqueCondition")]
-        [DataRow(InvalidContext_TextAfterColon, DisplayName = "InvalidContext_TextAfterColon")]
+        [DataRow(InvalidContexts.MissingName,                DisplayName = "InvalidContext_MissingName")]
+        [DataRow(InvalidContexts.MultipleParamsDeclarations, DisplayName = "InvalidContext_MultipleParamsDeclarations")]
+        [DataRow(InvalidContexts.MultipleRootConditions,     DisplayName = "InvalidContext_MultipleRootConditions")]
+        [DataRow(InvalidContexts.MultipleUniqueConditions,   DisplayName = "InvalidContext_MultipleUniqueConditions")]
+        [DataRow(InvalidContexts.ParamsAfterRootCondition,   DisplayName = "InvalidContext_ParamsAfterRootCondition")]
+        [DataRow(InvalidContexts.ParamsAfterUniqueCondition, DisplayName = "InvalidContext_ParamsAfterUniqueCondition")]
+        [DataRow(InvalidContexts.TextAfterColon,             DisplayName = "InvalidContext_TextAfterColon")]
         [DataTestMethod]
         public void InvalidContextsThrowException(string contextDeclaration)
         {
