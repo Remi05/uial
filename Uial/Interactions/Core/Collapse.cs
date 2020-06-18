673d6be7593dd850cc240ee2d4ceb7477d26e609
Remi Pelletier <remipelletier5@gmail.com>
2020-06-18 02:44:45 -0700

    Added Expand and Collapse core interactions

---
+++ Uial/Interactions/Core/Collapse.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Automation;
+using Uial.Contexts;
+
+namespace Uial.Interactions.Core
+{
+    public class Collapse : AbstractPatternInteraction<ExpandCollapsePattern>, IInteraction
+    {
+        public const string Key = "Collapse";
+
+        public override string Name => Key;
+        protected override AutomationPattern AutomationPattern => ExpandCollapsePattern.Pattern;
+
+        public Collapse(IContext context) : base(context) { }
+
+        public override void Do()
+        {
+            base.Do();
+            Pattern.Collapse();
+        }
+    }
+}
