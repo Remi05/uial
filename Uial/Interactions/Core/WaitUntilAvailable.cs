8794e4da7f631ef06d87bac927ef9f13b7af81b9
Remi Pelletier <remipelletier5@gmail.com>
2020-06-17 20:20:02 -0700

    Added WaitUntilAvailable core interaction

---
+++ Uial/Interactions/Core/WaitUntilAvailable.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using Uial.Contexts;
+
+namespace Uial.Interactions.Core
+{
+    public class WaitUntilAvailable : AbstractInteraction, IInteraction
+    {
+        public const string Key = "WaitUntilAvailable";
+
+        public override string Name => Key;
+
+        public WaitUntilAvailable(IContext context) : base(context) { }
+
+        public override void Do()
+        {
+            base.Do();
+            while (!Context.IsAvailable())
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
