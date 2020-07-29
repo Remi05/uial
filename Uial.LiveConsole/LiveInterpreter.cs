964326a0e2f47885dce456555c672d4609aba8cf
Remi Pelletier <remipelletier5@gmail.com>
2020-07-29 17:00:33 -0400

    Added LiveInterpreter class

---
+++ Uial.LiveConsole/LiveInterpreter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Automation;
+using Uial.Conditions;
+using Uial.Contexts;
+using Uial.Interactions;
+using Uial.Parsing;
+using Uial.Scopes;
+
+namespace Uial.LiveConsole
+{
+    public class LiveInterpreter
+    {
+        protected TextReader InputStream { get; set; }
+        protected TextWriter OutputStream { get; set; }
+        protected ScriptParser Parser { get; set; } = new ScriptParser();
+
+        public LiveInterpreter(TextReader inputStream, TextWriter outputStream)
+        {
+            if (inputStream == null)
+            {
+                throw new ArgumentNullException("inputStream");
+            }
+            InputStream = inputStream;
+            OutputStream = outputStream;
+        }
+
+        public void Run()
+        {
+            var script = new Script();
+            var rootScope = new RuntimeScope(new DefinitionScope(), new Dictionary<string, string>());
+            var rootContext = new RootContext(rootScope);
+
+            while (true)
+            {
+                string line = InputStream.ReadLine();
+                if (Parser.IsImport(line))
+                {
+                    script.AddScript(Parser.ParseRepoImport(line));
+                }
+                else if (Parser.IsBaseInteraction(line))
+                {
+                    IBaseInteractionDefinition baseInteractionDefinition = Parser.ParseBaseInteractionDefinition(line);
+                    IInteraction interaction = baseInteractionDefinition.Resolve(rootContext, rootScope);
+                    interaction.Do();
+                }
+                else if (Parser.IsCondition(line))
+                {
+                    IConditionDefinition conditionDefinition = Parser.ParseConditionDefinition(line);
+                    Condition condition = conditionDefinition.Resolve(rootScope);
+                    AutomationElement element = AutomationElement.RootElement.FindFirst(TreeScope.Subtree, condition);
+                    OutputElementInfo(element);
+                }
+                else if (Parser.IsBaseContext(line))
+                {
+                    IBaseContextDefinition baseContextDefinition = Parser.ParseBaseContextDefinition(line);
+                    IContext context = baseContextDefinition.Resolve(rootContext, rootScope);
+                    OutputElementInfo(context.RootElement);
+                }
+            }
+        }
+
+        protected void OutputElementInfo(AutomationElement automationElement)
+        {
+            OutputStream.WriteLine($"[{Helper.GetConditionFromElement(automationElement)}]\n");
+        }
+    }
+}
--- Uial.LiveConsole/Program.cs
+++ Uial.LiveConsole/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Uial.LiveConsole
 {
@@ -10,6 +6,8 @@ namespace Uial.LiveConsole
     {
         static void Main(string[] args)
         {
+            var interpreter = new LiveInterpreter(Console.In, Console.Out);
+            interpreter.Run();
         }
     }
 }
