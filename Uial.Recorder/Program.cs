7d1ce99bac7adfe2c24750029357861001839407
Remi Pelletier <remipelletier5@gmail.com>
2020-08-16 02:50:51 -0400

    Cleaned up usings

--- Uial.Recorder/Program.cs
+++ Uial.Recorder/Program.cs
@@ -1,10 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Automation;
-using Uial.Conditions;
 
 namespace Uial.Recorder
 {
