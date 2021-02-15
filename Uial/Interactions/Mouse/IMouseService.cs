2ccee2b4c1ae6c5078f1c329e3c48ab5fe9672b8
Remi Pelletier <remipelletier5@gmail.com>
2021-02-14 18:51:22 -0800

    Added mouse interaction provider

---
+++ Uial/Interactions/Mouse/IMouseService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uial.Interactions.Mouse
+{
+    public interface IMouseService
+    {
+        void Click();
+        void DoubleClick();
+        void Drag(int xOffset, int yOffset);
+        void DragTo(int xPosition, int yPosition);
+        void Move(int xOffset, int yOffset);
+        void MoveTo(int xPosition, int yPosition);
+        void Press();
+        void Release();
+    }
+}
---
+++ Uial/Interactions/Mouse/MouseInteractionProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Uial.Contexts;
+using Uial.Scopes;
+
+namespace Uial.Interactions.Mouse
+{
+    public class MouseInteractionProvider : IInteractionProvider
+    {
+        protected IMouseService MouseService { get; set; }
+
+        protected IDictionary<string, IInteraction> KnownInteractions = new Dictionary<string, IInteraction>();
+
+        public MouseInteractionProvider(IMouseService mouseService)
+        {
+            MouseService = mouseService;
+            KnownInteractions.Add("Click", new SkeletonInteraction("Click", MouseService.Click));
+            KnownInteractions.Add("DoubleClick", new SkeletonInteraction("DoubleClick", MouseService.DoubleClick));
+            //KnownInteractions.Add("Drag", new SkeletonInteraction("Drag", MouseService.Drag));
+            //KnownInteractions.Add("DragTo", new SkeletonInteraction("DragTo", MouseService.DragTo));
+            //KnownInteractions.Add("Move", new SkeletonInteraction("Move", MouseService.Move));
+            //KnownInteractions.Add("MoveTo", new SkeletonInteraction("MoveTo", MouseService.MoveTo));
+            KnownInteractions.Add("Press", new SkeletonInteraction("Press", MouseService.Press));
+            KnownInteractions.Add("Release", new SkeletonInteraction("Release", MouseService.Release));
+        }
+
+        public bool IsKnownInteraction(string interactionName)
+        {
+            return KnownInteractions.ContainsKey(interactionName);
+        }
+
+        public IInteraction GetInteractionByName(IContext context, RuntimeScope scope, string interactionName, IEnumerable<string> paramValues)
+        {   
+            if (!IsKnownInteraction(interactionName))
+            {
+                throw new InteractionUnavailableException(interactionName);
+            }
+            return KnownInteractions[interactionName];
+        }
+    }
+}
