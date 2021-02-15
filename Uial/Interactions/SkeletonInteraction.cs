8149757f22ce4c5444057b7b409e903159175ce9
Remi Pelletier <remipelletier5@gmail.com>
2021-02-14 18:51:22 -0800

    Added mouse interaction provider

---
+++ Uial/Interactions/SkeletonInteraction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uial.Interactions
+{
+    public class SkeletonInteraction : IInteraction
+    {
+        public string Name { get; private set; }
+        private Action Action { get; set; }
+
+        public SkeletonInteraction(string name, Action action)
+        {
+            if (name == null || action == null)
+            {
+                throw new ArgumentNullException(name == null ? nameof(name) : nameof(action));
+            }
+            Name = name;
+            Action = action;
+        }
+
+        public void Do()
+        {
+            Action();
+        }
+    }
+}
