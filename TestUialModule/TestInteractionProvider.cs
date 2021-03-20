9ef633f8f8c16a0e5a07caa931ca28b2da579351
Remi Pelletier <remipelletier5@gmail.com>
2021-03-20 14:13:29 -0700

    Added example module project

---
+++ TestUialModule/TestInteractionProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uial.Contexts;
+using Uial.Interactions;
+using Uial.Scopes;
+
+namespace TestUialModule
+{
+    public class TestInteractionProvider : IInteractionProvider
+    {
+        public bool IsKnownInteraction(string interactionName)
+        {
+            return interactionName == nameof(TestModuleInteraction);
+        }
+
+        public IInteraction GetInteractionByName(IContext context, RuntimeScope scope, string interactionName, IEnumerable<string> paramValues)
+        {
+            if (!IsKnownInteraction(interactionName))
+            {
+                throw new InteractionUnavailableException(interactionName);
+            }
+            return new TestModuleInteraction();
+        }
+    }
+}
---
+++ TestUialModule/TestModuleInteraction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Uial.Interactions;
+
+namespace TestUialModule
+{
+    public class TestModuleInteraction : IInteraction
+    {
+        public string Name => "TestModuleInteraction";
+
+        public void Do()
+        {
+            Console.WriteLine($"Hello from {nameof(TestModuleInteraction)}");
+        }
+    }
+}
