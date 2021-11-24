1dc69492f0d59b0a5d9bc01d57ccbd6485ebdde4
Remi Pelletier <remipelletier5@gmail.com>
2021-11-24 02:28:31 -0800

    Made ConditionDefinition abstract

--- Uial.Definitions/ConditionDefinition.cs
+++ Uial.Definitions/ConditionDefinition.cs
@@ -1,29 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
+﻿
 namespace Uial.Definitions
 {
-    public class ConditionDefinition
-    {
-        public IEnumerable<PropertyConditionDefinition> PropertyConditions { get; private set; }
-
-        public ConditionDefinition(IEnumerable<PropertyConditionDefinition> propertyConditions)
-        {
-            if (propertyConditions == null)
-            {
-                throw new ArgumentNullException(nameof(propertyConditions));
-            }
-            if (propertyConditions.Count() == 0)
-            {
-                throw new ArgumentException($"Parameter \"{nameof(propertyConditions)}\" must contain at least one ConditionDefinition.");
-            }
-            PropertyConditions = propertyConditions;
-        }
-
-        public override string ToString()
-        {
-            return $"{string.Join(", ", PropertyConditions)}";
-        }
-    }
+    public abstract class ConditionDefinition { }
 }
