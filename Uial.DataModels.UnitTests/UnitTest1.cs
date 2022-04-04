01a64d597341e230c1bda31c38e75799c3271e7a
Remi Pelletier <remipelletier5@gmail.com>
2022-04-04 00:37:39 -0700

    Added Uial.DataModels.UnitTests project

---
+++ Uial.DataModels.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Uial.DataModels.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
