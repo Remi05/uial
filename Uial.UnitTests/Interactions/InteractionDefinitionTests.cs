aa6a468b187f1a9ef0b942a461dac3aa6afe438e
Remi Pelletier <remipelletier5@gmail.com>
2022-04-04 00:22:06 -0700

    Added InteractionDefinitionTests

---
+++ Uial.UnitTests/Interactions/InteractionDefinitionTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uial.DataModels;
+
+namespace Uial.UnitTests.Interactions
+{
+    [TestClass]
+    public class InteractionDefinitionTests
+    {
+        [TestMethod]
+        public void VerifyNameIsSet()
+        {
+            // Arrange
+            string expectedName = "TestInteractionDefinition";
+
+            // Act
+            var interactionDefinition = new InteractionDefinition(new DefinitionScope(), expectedName, new List<string>(), new List<BaseInteractionDefinition>());
+
+            // Assert
+            Assert.AreEqual(expectedName, interactionDefinition.Name);
+        }
+    }
+}
