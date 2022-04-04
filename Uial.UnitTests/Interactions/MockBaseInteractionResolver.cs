28867d94c772359b0056a98c4d14d5afbfc5257f
Remi Pelletier <remipelletier5@gmail.com>
2022-04-04 00:22:25 -0700

    Added MockBaseInteractionResolver

---
+++ Uial.UnitTests/Interactions/MockBaseInteractionResolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Uial.Contexts;
+using Uial.DataModels;
+using Uial.Interactions;
+using Uial.Values;
+
+namespace Uial.UnitTests.Interactions
+{
+    internal class MockBaseInteractionResolver : IBaseInteractionResolver
+    {
+        public IDictionary<BaseInteractionDefinition, IInteraction> InteractionsMap { get; protected set; } = new Dictionary<BaseInteractionDefinition, IInteraction>();
+
+        public List<BaseInteractionDefinition> ResolvedBaseInteractions { get; private set; } = new List<BaseInteractionDefinition>();
+        public IContext LastPassedParentContext { get; private set; }
+        public IReferenceValueStore LastPassedValueStore { get; private set; }
+
+        public IInteraction Resolve(BaseInteractionDefinition baseInteractionDefinition, IContext parentContext, IReferenceValueStore referenceValueStore)
+        {
+            ResolvedBaseInteractions.Add(baseInteractionDefinition);
+            LastPassedParentContext = parentContext;
+            LastPassedValueStore = referenceValueStore;
+            return InteractionsMap.ContainsKey(baseInteractionDefinition) ? InteractionsMap[baseInteractionDefinition] : null;
+        }
+    }
+}
