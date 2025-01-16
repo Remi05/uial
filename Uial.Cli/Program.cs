b17c5085b67443faeae247a35904626ce2866b91
Rémi Pelletier <remipelletier5@gmail.com>
2025-01-16 13:53:52 -0500

    Made CLI DPI aware

--- Uial.Cli/Program.cs
+++ Uial.Cli/Program.cs
@@ -12,8 +12,17 @@ namespace Uial.Cli
 {
     class Program
     {
+        // Based on https://learn.microsoft.com/en-us/windows/win32/api/shellscalingapi/ne-shellscalingapi-process_dpi_awareness
+        private const int PROCESS_PER_MONITOR_DPI_AWARE = 2;
+
+        // Based on https://learn.microsoft.com/en-us/windows/win32/api/shellscalingapi/nf-shellscalingapi-setprocessdpiawareness
+        [System.Runtime.InteropServices.DllImport("Shcore.dll")]
+        private static extern bool SetProcessDpiAwareness(int dpiAwareness);
+
         static void Main(string[] args)
         {
+            SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
+
             string scriptFilePath;
             string scenarioName = null;
             string testName = null;
