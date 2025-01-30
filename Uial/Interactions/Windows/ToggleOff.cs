a413f861844ccfc0b3841325c518b089cb29c789
Rémi Pelletier <remipelletier5@gmail.com>
2025-01-29 22:53:14 -0500

    Added ToggleOn and ToggleOff interactions

---
+++ Uial/Interactions/Windows/ToggleOff.cs
@@ -0,0 +1,25 @@
+﻿using UIAutomationClient;
+using Uial.Contexts.Windows;
+
+using AutomationPatternIdentifier = System.Int32;
+
+namespace Uial.Interactions.Windows;
+
+internal class ToggleOff : AbstractPatternInteraction<IUIAutomationTogglePattern>, IInteraction
+{
+    public const string Key = "ToggleOff";
+
+    public override string Name => Key;
+    protected override AutomationPatternIdentifier AutomationPattern => UIA_PatternIds.UIA_TogglePatternId;
+
+    public ToggleOff(IWindowsVisualContext context) : base(context) { }
+
+    public override void Do()
+    {
+        base.Do();
+        if (Pattern.CurrentToggleState == ToggleState.ToggleState_On)
+        { 
+            Pattern.Toggle();
+        }
+    }
+}
---
+++ Uial/Interactions/Windows/ToggleOn.cs
@@ -0,0 +1,25 @@
+﻿using UIAutomationClient;
+using Uial.Contexts.Windows;
+
+using AutomationPatternIdentifier = System.Int32;
+
+namespace Uial.Interactions.Windows;
+
+internal class ToggleOn : AbstractPatternInteraction<IUIAutomationTogglePattern>, IInteraction
+{
+    public const string Key = "ToggleOn";
+
+    public override string Name => Key;
+    protected override AutomationPatternIdentifier AutomationPattern => UIA_PatternIds.UIA_TogglePatternId;
+
+    public ToggleOn(IWindowsVisualContext context) : base(context) { }
+
+    public override void Do()
+    {
+        base.Do();
+        if (Pattern.CurrentToggleState == ToggleState.ToggleState_Off)
+        { 
+            Pattern.Toggle();
+        }
+    }
+}
